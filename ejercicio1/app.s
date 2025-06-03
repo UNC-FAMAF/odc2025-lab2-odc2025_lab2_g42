661b79bde57b2714b5dd9b3f276b6ecbe8d67e86
Diego <diego.vilte@hotmail.com.ar>
2025-06-03 05:20:22 +0000

    Se agrego los carteles

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -29,12 +29,15 @@ loop0:
 	cbnz x2,loop1  // Si no es la última fila, salto
 	
 	bl DibujarEstrellas
+
 //Dibujar un Rectangulo
 
 	mov x21, #220
 	mov x22, #150
 	mov x23, #150
 	mov x24, #330
+	movz w25, 0x33 // Coloco el color
+	movk w25, 0x3333
 
 	bl DrawRect
 
@@ -42,6 +45,8 @@ loop0:
 	mov x22, #180
 	mov x23, #150
 	mov x24, #300
+	movz w25, 0x33 // Coloco el color
+	movk w25, 0x3333
 
 	bl DrawRect
 
@@ -49,8 +54,188 @@ loop0:
 	mov x22, #210	
 	mov x23, #150
 	mov x24, #270
+	movz w25, 0x33 // Coloco el color
+	movk w25, 0x3333
 
 	bl DrawRect 
+
+	// Dibujo un Cartel
+DibujoCartel:
+	mov x21, #245	//Posicion en fila: x
+	mov x22, #100	//Posicion en columna: y
+	mov x23, #100	//Ancho de la figura
+	mov x24, #50	//Alto de la figura
+
+	movz w25, 0x4040
+	movk w25, 0xFF40, lsl 16 // Color del cartel gris claro
+bl DrawRect
+
+	mov x21, #250	//Posicion en fila: x
+	mov x22, #140	//Posicion en columna: y
+	mov x23, #90	//Ancho de la figura
+	mov x24, #10	//Alto de la figura
+	//Color de la figura fondo
+	movz w25, 0x1919, lsl 16
+	movk w25, 0x34, lsl 0
+
+bl DrawRect
+
+// Dibujo el segundo cartel
+
+	mov x21, #405	//Posicion en fila: x
+	mov x22, #160	//Posicion en columna: y
+	mov x23, #120	//Ancho de la figura
+	mov x24, #50	//Alto de la figura
+	//Color de la figura 
+	movz w25, 0x4040
+	movk w25, 0xFF40, lsl 16 // Color del cartel gris claro
+
+bl DrawRect
+
+	mov x21, #410	//Posicion en fila: x
+	mov x22, #200	//Posicion en columna: y
+	mov x23, #110	//Ancho de la figura
+	mov x24, #10	//Alto de la figura
+	//Color de la figura fondo
+	movz w25, 0x1919, lsl 16
+	movk w25, 0x34, lsl 0
+
+bl DrawRect
+
+//Letras del cartel, Primero dibujo la letra 'O'
+
+	movz w25, 0xFFFF
+	movk w25, 0xFFFF, lsl 16
+
+	mov x21, #255  // eje x
+	mov x22, #110	// eje y
+	mov x23, #20 	// ancho
+	mov x24, #25	//alto
+bl DrawRect
+
+// Ahora dibujo la letra 'd'
+// Parte inferior
+	mov x21, #285
+	mov x22, #110
+	mov x23, #20
+	mov x24, #25
+bl DrawRect
+
+// Ahora dibujo la letra 'C'
+// Parte superior
+	mov x21, #315
+	mov x22, #110
+	mov x23, #20
+	mov x24, #25
+bl DrawRect
+
+// Dibujo los numeros del segundo cartel "2025"
+
+	mov x21, #420 // Eje x
+	mov x22, #170 // Eje y
+	mov x23, #20  // Ancho
+	mov x24, #25 // Alto
+bl DrawRect
+
+	mov x21, #445 // Eje x
+	mov x22, #170 // Eje y
+	mov x23, #20  // Ancho
+	mov x24, #25 // Alto
+bl DrawRect
+
+	mov x21, #470 // Eje x
+	mov x22, #170 // Eje y
+	mov x23, #20  // Ancho
+	mov x24, #25 // Alto
+bl DrawRect
+
+	mov x21, #495 // Eje x
+	mov x22, #170 // Eje y
+	mov x23, #20  // Ancho
+	mov x24, #25 // Alto
+bl DrawRect
+
+// Dibujo los detalles de las letras y numeros para darle su forma
+
+	movz w25, 0x4040	
+	movk w25, 0xFF40, lsl 16 // Color girs claro
+
+// Centro de 'o'
+	mov x21, #260
+	mov x22, #115
+	mov x23, #10
+	mov x24, #15
+bl DrawRect
+
+// Centro de 'd'
+	mov x21, #290
+	mov x22, #125
+	mov x23, #7
+	mov x24, #7
+bl DrawRect
+
+	mov x21, #285
+	mov x22, #105
+	mov x23, #12
+	mov x24, #17
+bl DrawRect
+
+// centro de la letra C
+	mov x21, #320
+	mov x22, #115
+	mov x23, #20
+	mov x24, #15
+bl DrawRect
+
+// Aca relleno los detalles de los numeros 
+// Centro del '0'
+	mov x21, #450
+	mov x22, #175
+	mov x23, #10
+	mov x24, #15
+bl DrawRect
+
+// Detalles del '2'
+	mov x21, #420
+	mov x22, #175
+	mov x23, #15
+	mov x24, #7
+bl DrawRect
+
+	mov x21, #425
+	mov x22, #185
+	mov x23, #15
+	mov x24, #7
+bl DrawRect
+
+// Detalles del segundo '2'
+	mov x21, #470
+	mov x22, #175
+	mov x23, #15
+	mov x24, #7
+bl DrawRect
+
+	mov x21, #475
+	mov x22, #185
+	mov x23, #15
+	mov x24, #7
+bl DrawRect
+
+// Detalles del '5'
+	mov x21, #500
+	mov x22, #175
+	mov x23, #15
+	mov x24, #7
+bl DrawRect
+
+	mov x21, #495
+	mov x22, #185
+	mov x23, #15
+	mov x24, #7
+bl DrawRect
+
+
+
 
 	//Dibujar luna
 	mov x21, #600 //Centro X de la luna
@@ -84,8 +269,7 @@ DrawRect:
 	mov x2, x22	//Y inicial
 	mov x3, x23	//ancho
 	mov x4, x24	//alto	
-	movz w10, 0x33 // Color Rectangulo	
-	movk w10, 0x3333
+	mov w10, w25 // color
 
 rect_loop_y:
 	mov x5, x1 //Resetear el valor de X para cada fila
